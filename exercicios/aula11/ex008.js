b4567eb5d5a7adf63940db680df9d9ad3a26063e
Lih Yasumura <lih.ysm567@gmail.com>
2022-02-02 10:25:12 -0300

    0809010

---
+++ exercicios/aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 55.5
+console.log(`A velocidade do seu carro é ${vel}Km/h.`)
+if (vel > 60) { // CONDIÇÃO SIMPLES
+    console.log(`Você ultrapassou o limite de velocidade. MULTADO!`)
+}
+console.log(`Dirija sempre usando cinto de segurança!`)
---
+++ exercicios/aula11/ex009.js
@@ -0,0 +1,15 @@
+var país = 'Japão'
+console.log(`Vivendo em ${país}.`)
+if (país == 'Brasil') { // SE O PAÍS É IGUAL A BRASIL
+    console.log(`Você é brasileiro!`)
+} else {
+    console.log(`Você é estrangeiro!`)
+}
+
+/*
+if (país != 'Brasil') { SE O PAÍS É DIFERENTE DE BRASIL
+    console.log(`Você é estrangeiro!`)
+} else {
+    console.log(`Você é brasileiro!`)
+}
+*/
