3eb4574c1cdff053fc74e17a8c746915746d6085
Lih Yasumura <lih.ysm567@gmail.com>
2022-02-09 09:21:32 -0300

    Create ex012.js

---
+++ exercicios/aula12/ex012.js
@@ -0,0 +1,11 @@
+var hora = 8
+console.log(`Agora são exatamente ${hora} horas.`)
+if (hora < 12) {
+    console.log('Bom Dia!')
+} else if (hora <= 18) {
+    console.log('Boa Tarde!')
+} else if (hora <= 19) {
+    console.log('Boa Noite!')
+} else {
+    console.log('Boa Madrugada!')
+}
