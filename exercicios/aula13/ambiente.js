be7a3ec2b77bb07124342d88737b020a6a7c9c03
Lih Yasumura <lih.ysm567@gmail.com>
2022-06-08 14:09:03 -0300

    .

---
+++ exercicios/aula13/ambiente.js
@@ -0,0 +1,29 @@
+// REPETIÇÕES COM TESTE NO FINAL - DO WHILE
+var c = 1
+do {
+    console.log(`${c} - Tudo bem?`)
+    c++ //c = c + 1
+} while (c <= 10)
+
+
+
+// REPETIÇÕES COM TESTE NO INÍCIO - WHILE
+/*
+var c = 1
+while (c <= 10) {
+    console.log(`${c} - Tudo bem?`)
+    c++ //c = c + 1
+}
+*/
+
+
+
+
+/*
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+*/
