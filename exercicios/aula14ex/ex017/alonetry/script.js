aebb12c88a0733b1b60ed99ca97a02b31ec7fa55
Lih Yasumura <lih.ysm567@gmail.com>
2022-06-09 11:34:07 -0300

    .

--- exercicios/aula14ex/ex017/alonetry/script.js
+++ exercicios/aula14ex/ex017/alonetry/script.js
@@ -1,19 +1,17 @@
-function tabuada() {
+function calcular () {
     let num = window.document.getElementById('txtn')
     let tab = window.document.getElementById('seltab')
+
     if (num.value.length == 0) {
-        window.alert ('Digite um número!') 
+        window.alert ('DIGITE UM NÚMERO!')
     } else {
         let n = Number(num.value)
-        let c = 1
         tab.innerHTML = ''
-        while (c <= 10) {
+        for (let c = 1; c <= 10; c++) {
             let item = window.document.createElement('option')
             item.text = `${n} x ${c} = ${n*c}`
             item.value = `tab${c}`
             tab.appendChild(item)
-            c++
         }
     }
-    
 }
