976070975e4447c18cf272c6fe0768e3ae79a234
Lih Yasumura <lih.ysm567@gmail.com>
2022-06-09 11:34:53 -0300

    Update script.js

--- exercicios/aula14ex/ex017/alonetry/script.js
+++ exercicios/aula14ex/ex017/alonetry/script.js
@@ -1,12 +1,13 @@
 function calcular () {
     let num = window.document.getElementById('txtn')
     let tab = window.document.getElementById('seltab')
-
+    
     if (num.value.length == 0) {
         window.alert ('DIGITE UM NÚMERO!')
     } else {
         let n = Number(num.value)
         tab.innerHTML = ''
+        
         for (let c = 1; c <= 10; c++) {
             let item = window.document.createElement('option')
             item.text = `${n} x ${c} = ${n*c}`
