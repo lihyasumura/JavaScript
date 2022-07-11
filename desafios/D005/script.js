9a43b38de1b0166314713265db7315e8cc5a0a07
Lih Yasumura <lih.ysm567@gmail.com>
2022-07-11 13:11:09 -0300

    D005

---
+++ desafios/D005/script.js
@@ -0,0 +1,25 @@
+function converter() {
+    let distancia = Number(window.prompt (`Digite uma distância em metros (m)!`))
+
+    let km = distancia / 1000;
+    let kmo = km.toLocaleString()
+    let hm = distancia / 100;
+    let hmo = hm.toLocaleString()
+    let dam = distancia / 10;
+    let damo = dam.toLocaleString()
+    let dm = distancia * 10;
+    let dmo = dm.toLocaleString()
+    let cm = distancia * 100;
+    let cmo = cm.toLocaleString()
+    let mm = distancia * 1000;
+    let mmo = mm.toLocaleString()
+
+    res.innerHTML = 
+    `<strong><p>A distância de ${distancia.toLocaleString()} metros, corresponde a...</strong></p>` +
+    `<p>${kmo} quilômetros (Km).` +
+    `<p>${hmo} quilômetros (Hm).` +
+    `<p>${damo} quilômetros (Dam).` +
+    `<p>${dmo} quilômetros (dm).` +
+    `<p>${cmo} quilômetros (cm).` +
+    `<p>${mmo} quilômetros (mm).`
+}
