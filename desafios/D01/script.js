fc399e12e0977c19eb493bd3629e77d75ea443a8
Lih Yasumura <lih.ysm567@gmail.com>
2022-07-11 09:59:35 -0300

    d001

---
+++ desafios/D01/script.js
@@ -0,0 +1,11 @@
+function b1() {
+    window.alert `Você clicou no Botão 1!`
+}
+
+function b2() {
+    window.alert `Você clicou no Botão 2!`
+}
+
+function b3() {
+    window.alert `Você clicou no Botão 3!`
+}
