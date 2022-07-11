d0738a965e0afdaaf34400fd9a848d5f0e607b63
Lih Yasumura <lih.ysm567@gmail.com>
2022-07-11 14:32:38 -0300

    D009

---
+++ desafios/D009/script.js
@@ -0,0 +1,25 @@
+function calcular() {
+    let nome = window.prompt(`Qual é o nome do funcionário?`)
+    
+    let ganha = Number(window.prompt(`Qual é o salário de ${nome}?`))
+    
+    let g = ganha.toLocaleString('pt-br', {style: 'currency', currency: 'BRL'})
+    
+    let porcentagem = Number(window.prompt(`O saláro de ${nome} vai ser reajustado em qual porcentagem?`))
+    
+    let ganhara = ganha * 0.18
+    let g2 = ganhara.toLocaleString('pt-br', {style: 'currency', currency: 'BRL'})
+    
+    let novo = ganha * 0.18 + ganha
+    let n = novo.toLocaleString('pt-br', {style: 'currency', currency: 'BRL'})
+
+    res.innerHTML =
+    `<strong><p>${nome} recebeu um aumento salarial!</strong></p>` +
+
+    `<p>O salário atual era de ${g}.</p>` +
+
+    `<p>Com um aumento de ${porcentagem}%, o salário vai aumentar ${g2} no próximo mês.
+    </p>` +
+    
+    `<p>E a partir daí, ${nome} vai passar a ganhar ${n}.</p>`
+}
